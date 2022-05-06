21d06cfdc72fa1ade39d4d3662e72c1148ff2f97
awestendorp <awestendorp@transip.nl>
2022-05-06 19:16:49 +0200

    adds content to dialog fragment and connects it to button

--- app/src/main/java/nl/purpleheart/wiemoeterbierhalen/MainActivity.kt
+++ app/src/main/java/nl/purpleheart/wiemoeterbierhalen/MainActivity.kt
@@ -1,11 +1,20 @@
 package nl.purpleheart.wiemoeterbierhalen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addParticipantBtn = findViewById<Button>(R.id.addParticipant)
+
+        addParticipantBtn.setOnClickListener {
+            val addPDialog = newParticipantsDialogFragment()
+            val fm = supportFragmentManager
+            addPDialog.show(fm, "newParticipantsDialogFragment")
+        }
     }
 }
--- app/src/main/java/nl/purpleheart/wiemoeterbierhalen/newParticipantsDialogFragment.kt
+++ app/src/main/java/nl/purpleheart/wiemoeterbierhalen/newParticipantsDialogFragment.kt
@@ -9,6 +9,10 @@ import androidx.fragment.app.DialogFragment
 
 class newParticipantsDialogFragment : DialogFragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_new_participants, container, false)
 
-
+        return view
+    }
 }
