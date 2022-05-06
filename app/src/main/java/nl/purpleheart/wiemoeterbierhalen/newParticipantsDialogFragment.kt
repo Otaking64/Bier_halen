92a2cd7658ee26be06c4e3e3967ebfcda7f0da6b
awestendorp <awestendorp@transip.nl>
2022-05-06 22:42:38 +0200

    added initial logic for add name dialog

--- app/src/main/java/nl/purpleheart/wiemoeterbierhalen/newParticipantsDialogFragment.kt
+++ app/src/main/java/nl/purpleheart/wiemoeterbierhalen/newParticipantsDialogFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 
 class newParticipantsDialogFragment : DialogFragment() {
@@ -12,7 +14,26 @@ class newParticipantsDialogFragment : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_new_participants, container, false)
+        val participantName = view.findViewById<EditText>(R.id.addParticipantNameEditText)
+        val cancelBtn = view.findViewById<Button>(R.id.AddPDialogCancel)
+        val confirmBtn = view.findViewById<Button>(R.id.AddPDialogDialogConfirm)
+
+        cancelBtn.setOnClickListener {
+            dismiss()
+        }
+
+        confirmBtn.setOnClickListener {
+            addNameToList(participantName.text.toString())
+        }
 
         return view
+    }
+
+    private fun addNameToList(name: String) {
+        //get list from cache
+        //add name to list
+        //save list to cache
+
+        dismiss()
     }
 }
