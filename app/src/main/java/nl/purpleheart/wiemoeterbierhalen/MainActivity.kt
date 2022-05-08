cd76560dba6ccf2b0245d1aa2f3d00efcc740025
awestendorp <awestendorp@transip.nl>
2022-05-09 01:46:16 +0200

    adds main menu with new list button

--- app/src/main/java/nl/purpleheart/wiemoeterbierhalen/MainActivity.kt
+++ app/src/main/java/nl/purpleheart/wiemoeterbierhalen/MainActivity.kt
@@ -1,7 +1,11 @@
 package nl.purpleheart.wiemoeterbierhalen
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,22 @@ class MainActivity : AppCompatActivity() {
             val addPDialog = newParticipantsDialogFragment()
             val fm = supportFragmentManager
             addPDialog.show(fm, "newParticipantsDialogFragment")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.delete -> {
+                Toast.makeText(baseContext, "Delete button pressed", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
