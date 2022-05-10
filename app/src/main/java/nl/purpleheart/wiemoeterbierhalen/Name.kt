755c7a18b2d6ffd0f78278dd17253fa11195ed74
awestendorp <awestendorp@transip.nl>
2022-05-11 00:34:10 +0200

    adds Name class

---
+++ app/src/main/java/nl/purpleheart/wiemoeterbierhalen/Name.kt
@@ -0,0 +1,20 @@
+package nl.purpleheart.wiemoeterbierhalen
+
+class Name {
+    private var Name: String? = null
+
+    fun Name() {}
+
+    fun Name(name: String?) {
+        Name = name
+    }
+
+    fun getName(): String? {
+        return Name
+    }
+
+    fun setName(name: String) {
+        Name = name
+    }
+
+}
